08506e9fbe5c329c0aa3a641b3b961132a9a68f6
Santu Roy <roysantu2002@yahoo.com>
2020-11-07 21:17:47 +0530

    Feat: Fixed issued for WhoList component card disply

--- src/views/WhoList.js
+++ src/views/WhoList.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react"
-import Styles from "../utils/globalStyles"
-import { makeStyles } from "@material-ui/core/styles"
-import { withStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardMedia from "@material-ui/core/CardMedia"
-import CardContent from "@material-ui/core/CardContent"
-import Container from "@material-ui/core/Container"
-import Divider from "@material-ui/core/Divider"
-import Typography from "@material-ui/core/Typography"
-import data from "../data/influencer.json"
-import GridList from "@material-ui/core/GridList"
-import GridListTile from "@material-ui/core/GridListTile"
-import GridListTileBar from "@material-ui/core/GridListTileBar"
-import ListSubheader from "@material-ui/core/ListSubheader"
-import Grid from "@material-ui/core/Grid"
+import React, { useState, useEffect } from "react";
+import Styles from "../utils/globalStyles";
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import data from "../data/influencer.json";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Grid from "@material-ui/core/Grid";
 import * as getDataApi from "../utils/getDataApi";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import Fade from 'react-reveal/Fade'
-
+import Fade from "react-reveal/Fade";
 
 const styles = (theme) => ({
   root: {
     display: "flex",
+    flexGrow: 1,
     flexWrap: "wrap",
     justifyContent: "space-around",
     overflow: "hidden",
@@ -33,8 +33,9 @@ const styles = (theme) => ({
   },
   card: {
     padding: 8,
-    margnTop: 4,
-    maxWidth: 300,
+    marginLeft: 8,
+    margnTop: 8,
+    minWidth: 185,
     margin: "auto",
     transition: "0.3s",
     boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
@@ -74,20 +75,20 @@ const styles = (theme) => ({
 
 class WhoList extends React.Component {
   constructor(props) {
-    super()
-      this.state = {
-        influencerList: [],
-        loading: false,
-      };
+    super();
+    this.state = {
+      influencerList: [],
+      loading: false,
+    };
   }
   componentDidMount() {
     const influencerLocalData = [];
-    const influencerRemoteData = []
+    const influencerRemoteData = [];
 
-      data.map((postData) => {
-        influencerLocalData.push(postData);
-      });
-    
+    data.map((postData) => {
+      influencerLocalData.push(postData);
+    });
+
     //console.log(influencerData)
     // this.setState(() => ({
     //   influencerList: influencerLocalData,
@@ -120,7 +121,6 @@ class WhoList extends React.Component {
         }));
       }
 
-
       // querySnapshot.map((query) => {
       //   influencerRemoteData.push(query)
       // })
@@ -145,68 +145,79 @@ class WhoList extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const influencerList = this.state.influencerList
+    const influencerList = this.state.influencerList;
 
     const loadingDeatils = (
       <Container className={classes.loading} component="section">
         <Typography variant="h2" marked="center" align="center">
-        Be Inspired
+          Be Inspired
         </Typography>
         <CircularProgress color="secondary" align="center" />
       </Container>
     );
 
-    const influencerDetails = 
-    <Grid container className={classes.root} spacing={2}>
-    <Grid item xs={6}>
-      <Typography variant='h1' marked='center' align='center'>
-        Be Inspired
-      </Typography>
-    </Grid>
-    <Fade bottom cascade={true}>
-    <Grid item xs={12}>
-      <Grid container justify='center' spacing={2}>
-        {influencerList && influencerList.map((influencer) => (
-          <Card key={influencer.name} className={classes.card}>
-            <CardMedia  className={classes.media} image={influencer.img} />
-            <CardContent className={classes.content}>
-              <Typography
-                variant="h5"
-                gutterBottom
-              >
-                {influencer.name}
-              </Typography>
-              <Typography
-                className={"MuiTypography--heading"}
-                variant={"h6"}
-                gutterBottom
-              >
-                {influencer.birthplace}
-              </Typography>
-              <Typography
-                className={"MuiTypography--subheading"}
-                variant="subtitle1"
-              >
-                {influencer.contributions}
-              </Typography>
-              <Divider className={classes.divider} light />
-              <Grid
-                item
-                component={"a"}
-                rel='noopener noreferrer'
-                target='_blank'
-                href={influencer.ref}
-              >
-                <button
-                >Know More</button>
+    const influencerDetails = (
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h1" marked="center" align="center">
+            Be Inspired
+          </Typography>
+        </Grid>
+        <Fade bottom cascade={true}>
+          <Grid item xs={8}>
+            <Grid container className={classes.root} spacing={4}>
+              {influencerList &&
+                influencerList.map((influencer) => (
+                  <Grid item xs={8} md={4} sm={6} xl={3}>
+                    <Card key={influencer.name} className={classes.card}>
+                      <CardMedia
+                        className={classes.media}
+                        image={influencer.img}
+                      />
+                      <CardContent className={classes.content}>
+                        <Typography variant="h5" gutterBottom>
+                          {influencer.name}
+                        </Typography>
+                        <Typography
+                          className={"MuiTypography--heading"}
+                          variant={"h6"}
+                          gutterBottom
+                        >
+                          {influencer.birthplace}
+                        </Typography>
+                        <Typography
+                          className={"MuiTypography--subheading"}
+                          variant="subtitle1"
+                        >
+                          {influencer.contributions}
+                        </Typography>
+                        <Divider className={classes.divider} light />
+                        <Grid
+                          item
+                          component={"a"}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          href={influencer.ref}
+                        >
+                          <button>Know More</button>
+                        </Grid>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+ 
+            </Grid>
+          </Grid>
+          <Grid item xs={2}>
+        
+            <Grid container className={classes.root} spacing={4}>
+              <Grid item>     <Typography>Test</Typography></Grid>
+       
               </Grid>
-            </CardContent>
-          </Card>
-        ))}
+            </Grid>
+        </Fade>
       </Grid>
-    </Grid>
-    </Fade>
-  </Grid>
+    );
 
     return this.state.loading ? influencerDetails : loadingDeatils;
   }
